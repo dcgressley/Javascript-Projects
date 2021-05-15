db97e392f32cc4e13dd15ddf3713e5e8405c8d5c
dcgressley <dcgressley@gmail.com>
2021-05-14 20:15:18 -0500

    Update main.js
    Changed the function to use concatenated strings and added comments

--- Basic Javascript Projects/Project2_functions/js/main.js
+++ Basic Javascript Projects/Project2_functions/js/main.js
@@ -1,5 +1,6 @@
-function greenTime() {
-    var str = "This text is green!";
-    var result = str.fontcolor("green");
-    document.getElementById("Green_Text").innerHTML = result;
+function greenTime() { //declaring and naming a function with no parameters
+    var str = "This text"; //declaring, naming and assigning a value to a string
+    str += " is green!"; //using the += operator to concatenate two strings into one
+    var result = str.fontcolor("green"); //declaring, naming and assigning a string variable, as well as using a method to change its color
+    document.getElementById("Green_Text").innerHTML = result; //displaying the variable result where the element with id "Green_Text" is
 }

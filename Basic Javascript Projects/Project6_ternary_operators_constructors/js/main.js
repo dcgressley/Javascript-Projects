ba98d6061eff46d6a89942060e3dee5213be72ae
dcgressley <dcgressley@gmail.com>
2021-05-19 19:55:41 -0500

    Update index.html and main.js
    Added more code to these files to display the usage of constructor functions, nested functions and more

--- Basic Javascript Projects/Project6_ternary_operators_constructors/js/main.js
+++ Basic Javascript Projects/Project6_ternary_operators_constructors/js/main.js
@@ -1,6 +1,39 @@
-function rideFunction() {
-    var Height, canRide;
-    Height = document.getElementById("Height").value;
-    canRide = (Height < 52) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML=canRide + " to ride.";
+var counter = 1;
+
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+}
+
+function iPhone(Model, Data, Color) {
+    this.iPhone_Color = Color;
+    this.iPhone_Data = Data;
+    this.iPhone_Model = Model;
+}
+var Dan = new iPhone("7", "124GB", "Red");
+var Chandler = new iPhone("8", "64GB", "Blue");
+
+function newFunction(Name) {
+    document.getElementById("New_and_This").innerHTML =
+    "Dan has a " + Dan.iPhone_Color + " iPhone " + Dan.iPhone_Model + " that has " +
+    Dan.iPhone_Data + " of storage.";
+}
+
+function outer() {
+    document.getElementById("nestedFunction").innerHTML = count();
+    function count() {
+        function plusOne() {counter += 1;}
+        plusOne();
+        return counter;
+    }
 }

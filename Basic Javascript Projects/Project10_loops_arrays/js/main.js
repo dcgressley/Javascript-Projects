a06ccf568fa085afa50de50685aea915f90465bc
dcgressley <dcgressley@gmail.com>
2021-05-23 16:01:46 -0500

    Add new project files, update slideshow files
    Added files to fulfill requirements for async and defer keyword projects, as well as continued troubleshooting the slideshow files

---
+++ Basic Javascript Projects/Project10_loops_arrays/js/main.js
@@ -0,0 +1,7 @@
+function callLoop() {
+    X = 1;
+    while (X <= 15) {
+        X+=1;
+        document.write("The number is " + (X-1) + "<br>");
+    }
+}
---
+++ Basic Javascript Projects/defer.js
@@ -0,0 +1,1 @@
+alert(document.getElementById("par").innerHTML);

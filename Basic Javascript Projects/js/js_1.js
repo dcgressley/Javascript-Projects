62fa0c6fa1656822ba04e18b1ae092f880818647
dcgressley <dcgressley@gmail.com>
2021-05-30 13:33:43 -0500

    Add HTML_1.html and js_1.js
    Added new files to construct a page that displayed the use of a switch statement

---
+++ Basic Javascript Projects/js/js_1.js
@@ -0,0 +1,28 @@
+function foodFunction() {
+    var foodOutput;
+    var foods = document.getElementById("foodInput").value;
+    var foodString = " is a great choice!";
+    switch(foods) {
+        case "Spaghetti":
+            foodOutput = "Spaghetti" + foodString;
+        break;
+        case "Hamburger":
+            foodOutput = "Hamburger" + foodString;
+        break;
+        case "Cheeseburger":
+            foodOutput = "Cheeseburger" + foodString;
+        break;
+        case "Cereal":
+            foodOutput = "Cereal" + foodString;
+        break;
+        case "Pancakes":
+            foodOutput = "Pancakes" + foodString;
+        break;
+        case "Waffles":
+            foodOutput = "Waffles" + foodString;
+        break;
+        default:
+            foodOutput = "Please enter a food exactly as written on the above list.";
+    }
+    document.getElementById("output").innerHTML = foodOutput;
+}
